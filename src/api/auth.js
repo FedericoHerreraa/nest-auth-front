2e4374e0e65b78f31bd71446e82880480ec5b006
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-10 18:45:09 -0300

    commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,3 +3,5 @@ import axios from 'axios'
 export const loginRequest = async (finalUser) => axios.post('https://nest-auth-alg9.onrender.com/auth/login', finalUser)
 
 export const registerRequest = async (userFinal) => axios.post('https://nest-auth-alg9.onrender.com/auth/register', userFinal)
+
+export const profileRequest = async () => axios.get('https://nest-auth-alg9.onrender.com/auth/profile', )
