3b52b06486af44eeaea04b4808db198d0dc1d660
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-11 16:29:53 -0300

    cmmiy

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const loginRequest = async (finalUser) => axios.post('https://nest-auth-alg9.onrender.com/auth/login', finalUser)
+export const loginRequest = async (finalUser) => axios.post('https://nest-auth-alg9.onrender.com/auth/login', finalUser, { withCredentials: true })
 
-export const registerRequest = async (userFinal) => axios.post('https://nest-auth-alg9.onrender.com/auth/register', userFinal)
+export const registerRequest = async (userFinal) => axios.post('https://nest-auth-alg9.onrender.com/auth/register', userFinal, { withCredentials: true })
 
-export const profileRequest = async () => axios.get('https://nest-auth-alg9.onrender.com/auth/profile', )
+export const profileRequest = async () => axios.get('https://nest-auth-alg9.onrender.com/auth/profile', { withCredentials: true })
