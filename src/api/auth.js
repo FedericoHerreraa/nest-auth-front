366b448bd43e1421d5449a8f30e9cb2d52d14a9e
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-12 09:21:04 -0300

    commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 export const loginRequest = async (finalUser) => {
     const res = await axios.post('https://nest-auth-alg9.onrender.com/auth/login', finalUser, { withCredentials: true })
-    console.log(res)
+    if (res.data && res.data.token) {
+        const token = res.data.token
+        Cookies.set('tu_cookie_nombre', token, { expires: 7 });
+
+        return token
+    } else {
+        console.log('No se encontro el token en la respuesta')
+        return null
+    }
 }
 
 export const registerRequest = async (userFinal) => axios.post('https://nest-auth-alg9.onrender.com/auth/register', userFinal, { withCredentials: true })
