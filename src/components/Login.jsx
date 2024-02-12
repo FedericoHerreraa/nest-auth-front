6345e93bf819853b97b60407dc65b4fe269fb203
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-12 09:59:23 -0300

    f

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,8 +23,8 @@ const Login = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="email"/>
-                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="contra"/>
+                <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="email"/>
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="contra"/>
                 <input type="submit" value='Iniciar sesion'/>
             </form>
         </div>
