82c74a3bff28676111b25deb3022cc1cbad9a08e
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-13 13:49:27 -0300

    f

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,21 +4,28 @@ import { useAuth } from "../context/authContext"
 const Profile = () => {
   const { user, profile, loading } = useAuth()
 
-  useEffect(async () => {
-    profile()
-  }, [])
+  const response = profile()
+  console.log(response)
 
-  if (loading) {
-    return <p>Loading...</p>
-  }
-
-  return (
+  if (response) {
+    return (
       <div>
           <h1>Profile</h1>
           <h2>{user.email}</h2>
           <p>{user.username}</p>
       </div>
-  )
+    )
+  } else {
+    return <p>Loading...</p>
+  }
+
+  // if (loading) {
+    
+  // }
+
+  // return (
+      
+  // )
 }
 
 export default Profile
