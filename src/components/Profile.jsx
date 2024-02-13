83ebd80521f5bff0e6fe47ac19dcf2ded40a9cdb
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-13 11:16:10 -0300

    f

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,27 +6,27 @@ const Profile = () => {
     // const { user } = useAuth()
     const [perfil, setPerfil] = useState(null);
 
-    // useEffect(async () => {
-    //     const obtenerPerfil = async () => {
-    //         try {
-    //           const perfilObtenido = await profileRequest();
-    //           setPerfil(perfilObtenido);
-    //         } catch (error) {
-    //           console.error('Error al obtener el perfil:', error);
-    //         }
-    //       };
+    useEffect(async () => {
+        const obtenerPerfil = async () => {
+            try {
+              const perfilObtenido = await profileRequest();
+              setPerfil(perfilObtenido);
+            } catch (error) {
+              console.error('Error al obtener el perfil:', error);
+            }
+          };
       
-    //       obtenerPerfil();
-    // }, [])  
+          obtenerPerfil();
+    }, [])  
 
-    // if (!perfil) {
-    //     return <p>No se pudo obtener el perfil del usuario.</p>;
-    // }
+    if (!perfil) {
+        return <p>No se pudo obtener el perfil del usuario.</p>;
+    }
 
     return (
         <div>
             <h1>Profile</h1>
-            {/* <h2>{perfil.email}</h2> */}
+            <h2>{perfil.email}</h2>
         </div>
     )
 }
